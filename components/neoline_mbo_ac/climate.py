e0bb99ef511011ffefce46444023e548da9071c1
krupnfs <49646693+krupnfs@users.noreply.github.com>
2026-08-29 20:19:30 +0400

    commit

--- components/neoline_mbo_ac/climate.py
+++ components/neoline_mbo_ac/climate.py
@@ -5,6 +5,7 @@ from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart"]
 
+# ВНИМАНИЕ: Проверьте нижние подчеркивания здесь!
 neoline_mbo_ac_ns = cg.esphome_ns.namespace("neoline_mbo_ac")
 NeolineMBOACClimate = neoline_mbo_ac_ns.class_(
     "NeolineMBOACClimate", climate.Climate, cg.Component, uart.UARTDevice
@@ -15,7 +16,6 @@ CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
         cv.GenerateID(): cv.declare_id(NeolineMBOACClimate),
     }
 ).extend(uart.UART_DEVICE_SCHEMA)
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
